abdc5dd97ea3558713141ef90d01c0a488024acd
thanai <62784012+thanaism@users.noreply.github.com>
2022-04-12 19:11:13 +0900

    Temporary implementation: call message API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    (async function () {
+      type Message = { text: string };
+      const messageResponse: AxiosResponse<Message> = await axios.post(
+        `/api/message`,
+        { name: "Sir" }
+      );
+      const { text } = messageResponse.data;
+      setData(text);
+    })();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +32,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>{data}</div>
       </header>
     </div>
   );
