a2a5e724b41fb445833286b16b7815b22c1681cd
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-04-23 19:34:07 +0900

    #11 Add a form to input custom alias

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -4,9 +4,7 @@ import { Header } from 'semantic-ui-react';
 const Heading: VFC = () => (
   <Header as="h1" size="huge" inverted textAlign="left">
     Thanai URL
-    <Header.Subheader>
-      The very best site to make URLs super short.
-    </Header.Subheader>
+    <Header.Subheader>Make URLs super short.</Header.Subheader>
   </Header>
 );
 
