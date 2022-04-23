4525c64eb0f7e31820627b20906a12734d9fcfd6
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-04-24 00:31:28 +0900

    Fix Ad component

--- src/Ad.tsx
+++ src/Ad.tsx
@@ -1,23 +1,30 @@
-import { VFC } from 'react';
+import { useEffect, VFC } from 'react';
 import { Advertisement } from 'semantic-ui-react';
 
-const Ad: VFC = () => (
-  <Advertisement unit="medium rectangle" centered>
-    <script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2138626283121213"
-      crossOrigin="anonymous"
-    />
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-2138626283121213"
-      data-ad-slot="1571821525"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
-    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-  </Advertisement>
-);
+interface AdsByGoogleWindow extends Window {
+  adsbygoogle: any; // eslint-disable-line
+}
+
+declare const window: AdsByGoogleWindow;
+
+const Ad: VFC = () => {
+  useEffect(() => {
+    if (window.adsbygoogle && process.env.NODE_ENV !== 'development')
+      window.adsbygoogle.push({}); // eslint-disable-line
+  }, []);
+
+  return (
+    <Advertisement unit="large rectangle">
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-2138626283121213"
+        data-ad-slot="1571821525"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </Advertisement>
+  );
+};
 
 export default Ad;
