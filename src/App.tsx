d38d37280e505c2f9fd71ee68e59b1c490539299
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-04-24 12:07:03 +0900

    Create a login demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useEffect, useState, VFC } from 'react';
+import { VFC } from 'react';
 import InputForm from 'InputForm';
 import Heading from 'Heading';
-import axios, { AxiosResponse } from 'axios';
 import LoginButton from 'LoginButton';
 import { Container, Grid } from 'semantic-ui-react';
 import PreviewMessage from 'PreviewMessage';
 import AuthorLink from 'AuthorLink';
 import Ad from 'Ad';
 
-type ClientPricipal = {
-  clientPrincipal: {
-    identityProvider: 'aad' | 'github' | 'twitter';
-    userId: string;
-    userDetails: string;
-    userRoles: ['anonymous', 'authenticated'];
-    claims: [];
-  };
-};
-
 const App: VFC = () => {
-  const [username, setUsername] = useState('');
-  useEffect(() => {
-    void (async (): Promise<void> => {
-      await axios
-        .get(`/.auth/me`)
-        .then((res: AxiosResponse<ClientPricipal>) => {
-          setUsername(res.data.clientPrincipal.userDetails);
-        });
-    })();
-  }, []);
+  const username = 'test-user';
 
   return (
     <Container style={{ padding: '1rem' }}>
